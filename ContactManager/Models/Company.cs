7779dd7bdd74dca05c4aa98b0ce1b62bdaa6b1fc
heimanhon <heimanhon@yahoo.co.uk>
2016-02-07 01:05:14 +0800

    initial file add

---
+++ ContactManager/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Models
+{
+    public class Company
+    {
+        public string CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string Offers { get; set; }
+        public double Discount { get; set; }
+    }
+}
---
+++ ContactManager/Models/Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Models
+{
+    public class Member
+    {
+        public string UserName { get; set; }
+        public string MemberId { get; set; }
+        public string CompanyId { get; set; }
+        public Company Company { get; set; }        
+    }
+}
